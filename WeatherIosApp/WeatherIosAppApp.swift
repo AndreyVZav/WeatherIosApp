5b4e30ff58eb218c9423ae688a56da2f299dd112
AndreyVZav <zav.av@bk.ru>
2025-05-12 16:16:41 +0300

    start

---
+++ WeatherIosApp/WeatherIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIosAppApp.swift
+//  WeatherIosApp
+//
+//  Created by Андрей Завадский on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
